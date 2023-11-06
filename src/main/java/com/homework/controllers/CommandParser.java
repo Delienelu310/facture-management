a2696a6da24e1cbfc6a01d4606b57f271af6675f
Delienelu310 <seredoha.nets@gmail.com>
2023-11-06 02:37:55 +0100

    Implement Command Parser

--- src/main/java/com/homework/controllers/CommandParser.java
+++ src/main/java/com/homework/controllers/CommandParser.java
@@ -11,7 +11,20 @@ public class CommandParser{
     private String route;
     private Map<String, String> parameters = new HashMap<>();
 
+    //the typical command looks smth like this: /put/element/add?factureId=5;productId=3;quantity=10
     public void parse(String command){
+        String[] routeAndParams = command.split("?");
+        route = routeAndParams[0];
+
+        parameters = new HashMap<>();
+        if(routeAndParams.length == 1) return;
+
+        String[] paramsPairs = routeAndParams[1].split(";");
+        for(String pair : paramsPairs){
+            String[] keyAndValue = pair.split("=");
+            parameters.put(keyAndValue[0], keyAndValue[1]);
+        }
+
 
     }
 }
